0a77b8cd71c5cffab64dfd924ad01313e0aa7db9
rwoodpecker <rwoodpecker@users.noreply.github.com>
2022-01-21 19:55:37 +1100

    Update salarypersecond.py
    update variable names

--- salarypersecond.py
+++ salarypersecond.py
@@ -1,10 +1,10 @@
 import time
 
-annualSalary = 100000 # enter your annual post-tax salary here
-salaryPerSecond = annualSalary/220/7.5/60/60
-stime = 0
+annual_salary = 100000 # enter your annual post-tax salary here
+salary_per_second = annual_salary/220/7.5/60/60
+start_time = 0
 
 while True:
    time.sleep(1)
-   stime += 1
-   print(f" Earnings after: {time.strftime('%H:%M:%S', time.gmtime(stime))} ${salaryPerSecond*stime:.2f}", end="\r")
+   start_time += 1
+   print(f" Earnings after: {time.strftime('%H:%M:%S', time.gmtime(start_time))} ${salary_per_second*start_time:.2f}", end="\r")
