606711bdaedb6a08eb1e267def9bee8d35a40313
ellisxanthis <ellis.xanthis@gmail.com>
2022-01-24 11:05:56 +0800

    wording change to differentiate from salarypersecond

--- returns_per_second.py
+++ returns_per_second.py
@@ -6,7 +6,7 @@ import time
 from decimal import Decimal
 
 PORTFOLIO_SIZE = 180000
-REAL_ANNUAL_RETURN = Decimal(8.72/100)
+REAL_ANNUAL_RETURN = Decimal(7/100)
 
 yearly_return = PORTFOLIO_SIZE * REAL_ANNUAL_RETURN
 earnings_per_second = Decimal(yearly_return / (365*24*60*60))
@@ -14,4 +14,4 @@ seconds = 0
 while True:
    time.sleep(1)
    seconds += 1
-   print(f" Earnings after: {time.strftime('%H:%M:%S', time.gmtime(seconds))} ${earnings_per_second*seconds:.4f}", end="\r")
+   print(f" Returns after: {time.strftime('%H:%M:%S', time.gmtime(seconds))} ${earnings_per_second*seconds:.4f}", end="\r")
