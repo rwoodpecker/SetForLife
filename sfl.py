6da25f10434c5eef3d7a768a7f8b11d6585b7873
ellisxanthis <ellis.xanthis@gmail.com>
2022-01-24 09:04:28 +0800

    changed account?

--- sfl.py
+++ sfl.py
@@ -1,9 +1,9 @@
 """
-A tool to help calculate approximately how much you get paid in real terms over 20 years when you win Set for Life.
+A tool to help calculate how much you get paid in real terms over 20 years when you win Set for Life.
 
 Set for Life is a lottery with a jackpot that pays you out A$20,000 per month for 20 years.
 
-Inflation is appled at the end of each year.
+Inflation is appled at the end of each year as a depreciation to the payment amount. 
 """
 from decimal import Decimal
 
