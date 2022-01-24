67a45d1090c3811db860be095ece8bf67b38502d
ellisxanthis <ellis.xanthis@gmail.com>
2022-01-24 15:59:14 +0800

    wording

--- returns_per_second.py
+++ returns_per_second.py
@@ -1,5 +1,5 @@
 """
-Calculate expected earnings per second on your portfolio
+Calculate expected earnings per second on your portfolio.
 """
 
 import time
@@ -10,6 +10,7 @@ REAL_ANNUAL_RETURN = Decimal(7/100)
 
 yearly_return = PORTFOLIO_SIZE * REAL_ANNUAL_RETURN
 earnings_per_second = Decimal(yearly_return / (365*24*60*60))
+print(f"Your expected daily return is ${earnings_per_second*60*60*24:.2f}.")
 seconds = 0
 while True:
    time.sleep(1)
