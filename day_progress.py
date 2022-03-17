7665a46bdad88f58a92196edec43b54b068ff6d9
ellisxanthis <ellis.xanthis@gmail.com>
2022-03-17 12:27:23 +0800

    notification (macos)

--- day_progress.py
+++ day_progress.py
@@ -4,6 +4,8 @@ Calculate what % through the work day you are
 
 from datetime import datetime, date, timedelta
 import time
+import platform
+import os
 
 # Constants
 # Change this if your work day is longer or shorter than 8 hours
@@ -21,7 +23,17 @@ work_endtime = work_start_date_time + timedelta(seconds=SECONDS_IN_WORK_DAY)
 # Print how far through the day you are in %.
 while True:
     time.sleep(1)
+    progress = (
+        ((datetime.now() - work_start_date_time).seconds) * 100 / SECONDS_IN_WORK_DAY
+    )
     print(
-        f"{((datetime.now() - work_start_date_time).seconds) * 100 / SECONDS_IN_WORK_DAY:.3f}% through the day...",
+        f"{progress:.3f} % through the day...",
         end="\r",
     )
+    if progress >= 100:
+        # Show a notification (macOS)
+        if platform.system() == "Darwin":
+            os.system(
+                'osascript -e \'display notification "Work day over" with title "Work day over"\''
+            )
+        sys.exit()
