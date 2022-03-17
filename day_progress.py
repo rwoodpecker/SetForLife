8fbd5258d271b2ba221b2e2cfc169ee8449688a2
ellisxanthis <ellis.xanthis@gmail.com>
2022-03-17 12:33:31 +0800

    linux notifications

--- day_progress.py
+++ day_progress.py
@@ -8,34 +8,48 @@ import platform
 import os
 import sys
 
-# Constants
-# Change this if your work day is longer or shorter than 8 hours
-WORK_START_HOUR = 8
-HOURS_IN_WORK_DAY = 8
-SECONDS_IN_WORK_DAY = HOURS_IN_WORK_DAY * 60 * 60
 
-# Get work start time and end time
-today = date.today()
-work_start_date_time = datetime.combine(today, datetime.min.time()) + timedelta(
-    hours=WORK_START_HOUR
-)
-work_endtime = work_start_date_time + timedelta(seconds=SECONDS_IN_WORK_DAY)
+def day_progress():
+    # Constants
+    # Change this if your work day is longer or shorter than 8 hours
+    WORK_START_HOUR = 8
+    HOURS_IN_WORK_DAY = 8
+    SECONDS_IN_WORK_DAY = HOURS_IN_WORK_DAY * 60 * 60
 
-# Print how far through the day you are in %.
-while True:
-    progress = (
-        ((datetime.now() - work_start_date_time).seconds) * 100 / SECONDS_IN_WORK_DAY
+    # Get work start time and end time
+    today = date.today()
+    work_start_date_time = datetime.combine(today, datetime.min.time()) + timedelta(
+        hours=WORK_START_HOUR
     )
-    if progress >= 100:
-        # Show a notification (macOS)
-        if platform.system() == "Darwin":
-            os.system(
-                'osascript -e \'display notification "Work day over" with title "Work day over"\''
-            )
-        print("\nWORK DAY OVER!!!\n")
-        sys.exit()
-    print(
-        f"{progress:.3f} % through the day...",
-        end="\r",
-    )
-    time.sleep(1)
+    work_endtime = work_start_date_time + timedelta(seconds=SECONDS_IN_WORK_DAY)
+
+    # Print how far through the day you are in %.
+    while True:
+        progress = (
+            ((datetime.now() - work_start_date_time).seconds)
+            * 100
+            / SECONDS_IN_WORK_DAY
+        )
+        if progress >= 100:
+            notify_and_exit()
+        print(
+            f"{progress:.3f} % through the day...",
+            end="\r",
+        )
+        time.sleep(1)
+
+
+def notify_and_exit():
+    # Show a notification (macOS)
+    if platform.system() == "Darwin":
+        os.system(
+            'osascript -e \'display notification "Work day over" with title "Work day over"\''
+        )
+    if platform.system == "Linux":
+        os.system("notify-send 'Work day over'")
+    print("\nWORK DAY OVER!!!\n")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    day_progress()
