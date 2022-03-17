290aaf174d3f0b389cef6735a4c17bc03746e8ae
ellisxanthis <ellis.xanthis@gmail.com>
2022-03-17 12:21:22 +0800

    easier customise work start time and length of day

--- day_progress.py
+++ day_progress.py
@@ -6,18 +6,22 @@ from datetime import datetime, date, timedelta
 import time
 
 # Constants
-SECONDS_IN_WORK_DAY = 8 * 60 * 60
+# Change this if your work day is longer or shorter than 8 hours
+WORK_START_HOUR = 8
+HOURS_IN_WORK_DAY = 8
+SECONDS_IN_WORK_DAY = HOURS_IN_WORK_DAY * 60 * 60
 
-# Get the time at 8am today
+# Get work start time and end time
 today = date.today()
-work_start_date_time = datetime.combine(today, datetime.min.time()) + timedelta(hours=8)
+work_start_date_time = datetime.combine(today, datetime.min.time()) + timedelta(
+    hours=WORK_START_HOUR
+)
 work_endtime = work_start_date_time + timedelta(seconds=SECONDS_IN_WORK_DAY)
 
-
-start_time = 0
+# Print how far through the day you are in %.
 while True:
     time.sleep(1)
     print(
-        f"{((datetime.now() - work_start_date_time).seconds) * 100 / SECONDS_IN_WORK_DAY:.3f}% through the day",
+        f"{((datetime.now() - work_start_date_time).seconds) * 100 / SECONDS_IN_WORK_DAY:.3f}% through the day...",
         end="\r",
     )
